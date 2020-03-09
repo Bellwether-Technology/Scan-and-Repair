c2b386ec7b907274a30a1a24b1c9287d9ccc4f65
rwilliams-belltec <rwilliams@belltec.com>
2020-03-09 15:05:26 -0500

    Added StartComponentCleanup and documentation

--- ScanRepair.ps1
+++ ScanRepair.ps1
@@ -24,21 +24,38 @@ try {
 
 Write-Host "Phase 2: DISM Scan and Repair" -ForegroundColor Blue -BackgroundColor White
 try {
+
+	#Initial DISM scan to see if their is component corruption
 	$DISMState = Repair-WindowsImage -ScanHealth -Online
+
+	#Check results of initial scan
 	if ($DISMState.ImageHealthState -eq "Healthy") {
 		Write-Host "Windows image is in healthy state." -ForegroundColor Green
 		if ($DISMState.RestartNeeded -eq $True) {
 			Write-Host "Restart is required." -ForegroundColor Yellow
 		}
 	} else {
+
+		#If unhealthy state, attempt to repair (DISM /Online /Cleanup-image /RestoreHealth)
 		Write-Host "Windows image is in unhealthy state and needs to be repaired." -ForegroundColor Red
 		Write-Host "Attempting Windows image repair now..." -ForegroundColor Yellow
 		$DISMState = Repair-WindowsImage -RestoreHealth -Online
 		if ($DISMState.ImageHealthState -eq "Healthy") {
 			Write-Host "Windows image has been successfully repaired." -ForegroundColor Green
+		
 		} else {
+
+			#If /RestoreHealth doesn't work, run /StartComponentCleanup in last ditch effort
 			Write-Host "Windows image was not able to be repaired." -ForegroundColor Red
-		}
+			Write-Host "Beginning DISM's StartComponentCleanup..." -ForegroundColor Yellow
+			dism.exe /Online /Cleanup-Image /StartComponentCleanup
+			$DISMState = Repair-WindowsImage -ScanHealth -Online
+			if ($DISMState.ImageHealthState -eq "Healthy") {
+				Write-Host "Windows image is in healthy state." -ForegroundColor Green
+			} else {
+				Write-Host "This did not work" -ForegroundColor Red
+			}
+		}		
 		if ($DISMState.RestartNeeded -eq $True) {
 			Write-Host "Restart is required." -ForegroundColor Yellow
 		}
